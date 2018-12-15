d42a54cac1a0889652bf3c32a3af10d8dd12bfdd
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-15 18:33:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function iterativeLog(array){
+  array.forEach(`${index} + ${element}`);
+}
+
+function iterate(callback){
+  var evens = [2,4,6,8];
+  evens.forEach(callback);
+  return evens;
+}
+
+function doToArray (array, callback) {
+  array.forEach(callback);
+}
